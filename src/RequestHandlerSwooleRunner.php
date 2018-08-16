fb548bf238d15ef061184a14350c41105867ef59
huangzhhui <huangzhwork@gmail.com>
2018-08-16 21:44:38 +0800

    revert some format commit

--- src/RequestHandlerSwooleRunner.php
+++ src/RequestHandlerSwooleRunner.php
@@ -29,9 +29,11 @@ use function sprintf;
 
 /**
  * "Run" a request handler using Swoole.
+ *
  * The RequestHandlerRunner will marshal a request using the composed factory, and
  * then pass the request to the composed handler. Finally, it emits the response
  * returned by the handler using the Swoole emitter.
+ *
  * If the factory for generating the request raises an exception or throwable,
  * then the runner will use the composed error response generator to generate a
  * response, based on the exception or throwable raised.
@@ -43,68 +45,67 @@ class RequestHandlerSwooleRunner extends RequestHandlerRunner
      *
      * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
      */
-    public const DEFAULT_STATIC_EXTS
-        = [
-            '7z' => 'application/x-7z-compressed',
-            'aac' => 'audio/aac',
-            'arc' => 'application/octet-stream',
-            'avi' => 'video/x-msvideo',
-            'azw' => 'application/vnd.amazon.ebook',
-            'bin' => 'application/octet-stream',
-            'bmp' => 'image/bmp',
-            'bz' => 'application/x-bzip',
-            'bz2' => 'application/x-bzip2',
-            'css' => 'text/css',
-            'csv' => 'text/csv',
-            'doc' => 'application/msword',
-            'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-            'eot' => 'application/vnd.ms-fontobject',
-            'epub' => 'application/epub+zip',
-            'es' => 'application/ecmascript',
-            'gif' => 'image/gif',
-            'htm' => 'text/html',
-            'html' => 'text/html',
-            'ico' => 'image/x-icon',
-            'jpg' => 'image/jpg',
-            'jpeg' => 'image/jpg',
-            'js' => 'text/javascript',
-            'json' => 'application/json',
-            'mp4' => 'video/mp4',
-            'mpeg' => 'video/mpeg',
-            'odp' => 'application/vnd.oasis.opendocument.presentation',
-            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
-            'odt' => 'application/vnd.oasis.opendocument.text',
-            'oga' => 'audio/ogg',
-            'ogv' => 'video/ogg',
-            'ogx' => 'application/ogg',
-            'otf' => 'font/otf',
-            'pdf' => 'application/pdf',
-            'png' => 'image/png',
-            'ppt' => 'application/vnd.ms-powerpoint',
-            'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-            'rar' => 'application/x-rar-compressed',
-            'rtf' => 'application/rtf',
-            'svg' => 'image/svg+xml',
-            'swf' => 'application/x-shockwave-flash',
-            'tar' => 'application/x-tar',
-            'tif' => 'image/tiff',
-            'tiff' => 'image/tiff',
-            'ts' => 'application/typescript',
-            'ttf' => 'font/ttf',
-            'txt' => 'text/plain',
-            'wav' => 'audio/wav',
-            'weba' => 'audio/webm',
-            'webm' => 'video/webm',
-            'webp' => 'image/webp',
-            'woff' => 'font/woff',
-            'woff2' => 'font/woff2',
-            'xhtml' => 'application/xhtml+xml',
-            'xls' => 'application/vnd.ms-excel',
-            'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-            'xml' => 'application/xml',
-            'xul' => 'application/vnd.mozilla.xul+xml',
-            'zip' => 'application/zip',
-        ];
+    public const DEFAULT_STATIC_EXTS = [
+        '7z'    => 'application/x-7z-compressed',
+        'aac'   => 'audio/aac',
+        'arc'   => 'application/octet-stream',
+        'avi'   => 'video/x-msvideo',
+        'azw'   => 'application/vnd.amazon.ebook',
+        'bin'   => 'application/octet-stream',
+        'bmp'   => 'image/bmp',
+        'bz'    => 'application/x-bzip',
+        'bz2'   => 'application/x-bzip2',
+        'css'   => 'text/css',
+        'csv'   => 'text/csv',
+        'doc'   => 'application/msword',
+        'docx'  => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'eot'   => 'application/vnd.ms-fontobject',
+        'epub'  => 'application/epub+zip',
+        'es'    => 'application/ecmascript',
+        'gif'   => 'image/gif',
+        'htm'   => 'text/html',
+        'html'  => 'text/html',
+        'ico'   => 'image/x-icon',
+        'jpg'   => 'image/jpg',
+        'jpeg'  => 'image/jpg',
+        'js'    => 'text/javascript',
+        'json'  => 'application/json',
+        'mp4'   => 'video/mp4',
+        'mpeg'  => 'video/mpeg',
+        'odp'   => 'application/vnd.oasis.opendocument.presentation',
+        'ods'   => 'application/vnd.oasis.opendocument.spreadsheet',
+        'odt'   => 'application/vnd.oasis.opendocument.text',
+        'oga'   => 'audio/ogg',
+        'ogv'   => 'video/ogg',
+        'ogx'   => 'application/ogg',
+        'otf'   => 'font/otf',
+        'pdf'   => 'application/pdf',
+        'png'   => 'image/png',
+        'ppt'   => 'application/vnd.ms-powerpoint',
+        'pptx'  => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        'rar'   => 'application/x-rar-compressed',
+        'rtf'   => 'application/rtf',
+        'svg'   => 'image/svg+xml',
+        'swf'   => 'application/x-shockwave-flash',
+        'tar'   => 'application/x-tar',
+        'tif'   => 'image/tiff',
+        'tiff'  => 'image/tiff',
+        'ts'    => 'application/typescript',
+        'ttf'   => 'font/ttf',
+        'txt'   => 'text/plain',
+        'wav'   => 'audio/wav',
+        'weba'  => 'audio/webm',
+        'webm'  => 'video/webm',
+        'webp'  => 'image/webp',
+        'woff'  => 'font/woff',
+        'woff2' => 'font/woff2',
+        'xhtml' => 'application/xhtml+xml',
+        'xls'   => 'application/vnd.ms-excel',
+        'xlsx'  => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'xml'   => 'application/xml',
+        'xul'   => 'application/vnd.mozilla.xul+xml',
+        'zip'   => 'application/zip',
+    ];
 
     /**
      * @var array
@@ -149,6 +150,7 @@ class RequestHandlerSwooleRunner extends RequestHandlerRunner
      * A factory capable of generating an error response in the scenario that
      * the $serverRequestFactory raises an exception during generation of the
      * request instance.
+     *
      * The factory will receive the Throwable or Exception that caused the error,
      * and must return a Psr\Http\Message\ResponseInterface instance.
      *
@@ -184,17 +186,20 @@ class RequestHandlerSwooleRunner extends RequestHandlerRunner
             return $serverRequestFactory($request);
         };
 
-        $this->serverRequestErrorResponseGenerator
-            = function (Throwable $exception) use ($serverRequestErrorResponseGenerator) : ResponseInterface {
-            return $serverRequestErrorResponseGenerator($exception);
-        };
+        $this->serverRequestErrorResponseGenerator =
+            function (Throwable $exception) use ($serverRequestErrorResponseGenerator) : ResponseInterface {
+                return $serverRequestErrorResponseGenerator($exception);
+            };
 
         $this->server = $server;
 
         $this->allowedStatic = $config['static_files'] ?? self::DEFAULT_STATIC_EXTS;
         $this->docRoot = $config['options']['document_root'] ?? getcwd() . '/public';
         if (! file_exists($this->docRoot)) {
-            throw new Exception\InvalidConfigException(sprintf('The document_root %s does not exist. Please check your configuration.', $this->docRoot));
+            throw new Exception\InvalidConfigException(sprintf(
+                'The document_root %s does not exist. Please check your configuration.',
+                $this->docRoot
+            ));
         }
 
         $this->logger = $logger ? : new StdoutLogger();
@@ -304,10 +309,10 @@ class RequestHandlerSwooleRunner extends RequestHandlerRunner
         SwooleHttpResponse $response
     ) {
         $this->logger->info('{ts} - {remote_addr} - {request_method} {request_uri}', [
-            'ts' => date('Y-m-d H:i:sO', time()),
-            'remote_addr' => $request->server['remote_addr'],
+            'ts'             => date('Y-m-d H:i:sO', time()),
+            'remote_addr'    => $request->server['remote_addr'],
             'request_method' => $request->server['request_method'],
-            'request_uri' => $request->server['request_uri']
+            'request_uri'    => $request->server['request_uri']
         ]);
 
         if ($this->getStaticResource($request, $response)) {
@@ -344,9 +349,11 @@ class RequestHandlerSwooleRunner extends RequestHandlerRunner
     private function getStaticResource(
         SwooleHttpRequest $request,
         SwooleHttpResponse $response
-    ): bool {
+    ) : bool {
         $staticFile = $this->docRoot . $request->server['request_uri'];
-        if (! isset($this->cacheTypeFile[$staticFile]) && ! $this->cacheFile($staticFile)) {
+        if (! isset($this->cacheTypeFile[$staticFile])
+            && ! $this->cacheFile($staticFile)
+        ) {
             return false;
         }
 
