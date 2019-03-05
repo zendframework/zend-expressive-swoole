789ebacf4f2ba3df9e70b824e6042a96b025be8c
Matthew Weier O'Phinney <matthew@zend.com>
2019-03-05 12:04:04 -0600

    qa: updates year range for license docblocks of changed files
    Also adds them for new files.

--- test/Log/LoggerFactoryHelperTrait.php
+++ test/Log/LoggerFactoryHelperTrait.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @see       https://github.com/zendframework/zend-expressive-swoole for the canonical source repository
+ * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive-swoole/blob/master/LICENSE.md New BSD License
+ */
+
 declare(strict_types=1);
 
 namespace ZendTest\Expressive\Swoole\Log;
